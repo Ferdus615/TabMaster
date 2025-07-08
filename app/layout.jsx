07d497c93603bab20cc2ff78fbf0453fe6c0f525
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-07-08 12:19:31 +0600

    Navbar using  useState(not finished)

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,13 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header>
-          <nav>Navbar</nav>
-        </header>
+        <Navbar />
         <main>{children}</main>
-        <footer>
-          <p>© 2023 Ferdus Rhaman Khan</p>
-        </footer>
       </body>
     </html>
   );
