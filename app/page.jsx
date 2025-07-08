bd109daddf00cfd4b7746a68c2e259bcbabee585
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-07-09 01:52:34 +0600

    home page basic work done, font import done

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className=" flex flex-col items-center justify-center mb-5">
+        <Image
+          src="/logo-icon-w.png"
+          alt="TabMaster Logo"
+          width={150}
+          height={150}
+          className="mb-2"
+        />
+        <h1 className="font-semibold text-4xl">Welcome to TabMaster</h1>
+        <h3 className="font-light text-sm text-green-500">
+          by National Debate Federation Bangladesh(NDF BD)
+        </h3>
+      </div>
+      <a href="/registration">Register</a>
     </div>
   );
 }
