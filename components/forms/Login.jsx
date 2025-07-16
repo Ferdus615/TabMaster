7e50377dc16d561e8a8adf99eb07535f969bc0b8
FerdusWTV <ferdus.rahman@wtvglobal.com>
2025-07-16 22:46:12 +0600

    no code

--- components/forms/Login.jsx
+++ components/forms/Login.jsx
@@ -1,14 +1,28 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import { React, useState } from "react";
 
 const Login = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const logIsChecked = (event) => {
+    const myCheckeBox = event.target;
+    setIsChecked(myCheckeBox.checked);
+
+    if (myCheckeBox.checked) {
+      console.log(`The box is checked!`);
+    } else {
+      console.log(`The box is not checked!`);
+    }
+  };
+
   return (
     <form action="post" className="grid grid-cols-1 w-full max-w-sm gap-8">
       <Image
         src="/logo-wide-w.svg"
         alt="Registration"
-        width={150}
-        height={150}
+        width="150"
+        height="150"
       />
 
       <h1 className="text-2xl/8 font-medium">Log in to your profile</h1>
@@ -43,15 +57,18 @@ const Login = () => {
         <div>
           <label
             htmlFor="remember"
-            className="inline-flex cursor-pointer gap-4 items-center text-center"
+            className="inline-flex cursor-pointer gap-4 items-center text-center hover:cursor-pointer"
           >
             <input
               type="checkbox"
               id="remember"
               name="remember"
-              className="border-1 border-zinc-700 rounded-md p-2 bg-[#10172a]/40 hover:cursor-pointer"
+              checked={isChecked}
+              onChange={logIsChecked}
+              className="appearance-none sr-only peer p-[5px] border-2 rounded-sm"
             />
-            Remember me
+            <div className="p-[5px] border-2 rounded-sm peer-checked:bg-rose-500 peer-checked:border-rose-500 ease-in-out"></div>
+            <span className="text-sm">Remember me</span>
           </label>
         </div>
 
