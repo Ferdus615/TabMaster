05d71e3698c2bb860d6b22b172b489b40ae40211
FerdusWTV <ferdus.rahman@wtvglobal.com>
2025-07-25 21:02:38 +0600

    roles button style change

--- components/button/SButton.jsx
+++ components/button/SButton.jsx
@@ -7,9 +7,9 @@ function SButton({ id = "", text = "Click Me", onClick, className, selected }) {
       onClick={onClick}
       className={`${
         selected
-          ? "bg-sky text-persian"
-          : "bg-persian hover:bg-persian hover:text-white"
-      } px-2 py-1 rounded-full border-1 text-sm font-regular flex justify-center transition hover:cursor-pointer ${className}`}
+          ? "bg-sky text-persian hover:border-persian"
+          : "bg-persian hover:bg-persian border-zinc-border"
+      } px-2 py-1 rounded-full border-2 text-sm font-regular flex justify-center transition hover:cursor-pointer ${className}`}
     >
       {text}
     </button>
