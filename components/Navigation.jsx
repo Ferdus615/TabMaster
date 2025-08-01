5fe3e52a9b5011bcb76ae7c64f004cf853761ba3
FerdusWTV <ferdus.rahman@wtvglobal.com>
2025-08-01 23:01:16 +0600

    misc code

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -39,6 +39,87 @@ const Navigation = () => {
         <a href="#" className="hover:bg-blue-700 rounded-md p-2">
           Record
         </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a><a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
+        <a href="#" className="hover:bg-blue-700 rounded-md p-2">
+          Record
+        </a>
       </nav>
     </div>
   );
