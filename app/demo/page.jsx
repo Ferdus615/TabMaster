5aec56e2b5e74b522a4dfd45b977b61226102ad1
FerdusWTV <ferdus.rahman@wtvglobal.com>
2025-08-05 21:20:06 +0600

    demo team info tabel

---
+++ app/demo/page.jsx
@@ -0,0 +1,8 @@
+import TeamPage from "@/components/demoPage/TeamPage";
+import React from "react";
+
+const page = () => {
+  return <TeamPage />;
+};
+
+export default page;
---
+++ components/demoPage/TeamPage.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Team</th>
+          <th>Speakers</th>
+          <th>Round 1</th>
+          <th>Round 2</th>
+          <th>Round 3</th>
+          <th>Round 4</th>
+          <th>Round 5</th>
+          <th>Total Points</th>
+        </tr>
+        <tr>
+          <td>OIS A</td>
+          <td>Aryan, Saima, Tanvir</td>
+          <td>1st (3)</td>
+          <td>2nd (2)</td>
+          <td>1st (3)</td>
+          <td>3rd (1)</td>
+          <td>1st (3)</td>
+          <td>12</td>
+        </tr>
+        <tr>
+          <td>Maple B</td>
+          <td>Farzana, Jahid, Tazrian</td>
+          <td>2nd (2)</td>
+          <td>1st (3)</td>
+          <td>4th (0)</td>
+          <td>2nd (2)</td>
+          <td>3rd (1)</td>
+          <td>8</td>
+        </tr>
+        <tr>
+          <td>Sunnydale X</td>
+          <td>Zarin, Nehal, Rakib</td>
+          <td>3rd (1)</td>
+          <td>3rd (1)</td>
+          <td>2nd (2)</td>
+          <td>1st (3)</td>
+          <td>2nd (2)</td>
+          <td>9</td>
+        </tr>
+        <tr>
+          <td>Scholars Gold</td>
+          <td>Mahin, Rida, Ayaan</td>
+          <td>4th (0)</td>
+          <td>2nd (2)</td>
+          <td>1st (3)</td>
+          <td>2nd (2)</td>
+          <td>4th (0)</td>
+          <td>7</td>
+        </tr>
+        <tr>
+          <td>South Breeze Prime</td>
+          <td>Mehzabin, Arif, Nusaiba</td>
+          <td>1st (3)</td>
+          <td>4th (0)</td>
+          <td>3rd (1)</td>
+          <td>4th (0)</td>
+          <td>1st (3)</td>
+          <td>7</td>
+        </tr>
+        <tr>
+          <td>British Council Alpha</td>
+          <td>Laila, Rayhan, Nishat</td>
+          <td>2nd (2)</td>
+          <td>1st (3)</td>
+          <td>2nd (2)</td>
+          <td>1st (3)</td>
+          <td>2nd (2)</td>
+          <td>12</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default TeamPage;
