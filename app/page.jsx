ecfd8ae27da990f8693a0f79d0a442b29abd8b35
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-08-16 10:34:01 +0600

    new name components added

--- app/page.jsx
+++ app/page.jsx
@@ -6,12 +6,17 @@ import FinalCTA from "@/components/LandingPage/Final-CTA";
 import Roles from "@/components/LandingPage/Roles";
 import Footer from "@/components/LandingPage/Footer";
 import Tech from "@/components/LandingPage/Tech";
+import Name from "@/components/LandingPage/Name";
+import Hero_2 from "@/components/LandingPage/Hero_2";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
       <Navbar />
-      <Hero />
+      <Name />
+      <Hero_2 />
+      {/* <Hero /> */}
+
       <Feature />
       <Roles />
       <FinalCTA />
---
+++ components/LandingPage/Name.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Name = () => {
+  return (
+    <div className="mt-5 h-70">
+      <span className="name_style">TABMASTER</span>
+    </div>
+  );
+};
+
+// mt-5 h-70
+
+export default Name;
