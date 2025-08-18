f160cb041751c0989f02de31f9cb82e29776928f
ferdus615 <ferdus615@gmail.com>
2025-08-18 22:21:39 +0600

    vision page done

--- components/LandingPage/Vision.jsx
+++ components/LandingPage/Vision.jsx
@@ -4,7 +4,7 @@ const Vision = () => {
   return (
     <div className="mx-auto max-w-6xl mb-20">
       <div className="bg-persian/60 rounded-md text-colorText p-8">
-        <h1 className="fluid-text-3">
+        <h1 className="fluid-text-3 text-justify">
           OUR VISION IS A WORLD WHERE DEBATE IS ACCESSIBLE, EFFICIENT, AND
           DATA-DRIVEN. SMARTER TOURNAMENTS, EMPOWERED ADJUDICATORS, AND A
           SEAMLESS EXPERIENCE FOR EVERY DEBATER AND A LIFE LONG HISTORY.
