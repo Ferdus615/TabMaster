99c0d781c0a7589f495b318a5d604960574ba411
ferdus615 <ferdus615@gmail.com>
2025-08-20 23:08:00 +0600

    20-08-25

--- components/LandingPage/Footer.jsx
+++ components/LandingPage/Footer.jsx
@@ -4,48 +4,59 @@ import Link from "next/link";
 import ButtonV from "../Button/ButtonV";
 import ButtonR from "../Button/ButtonCTA";
 
+// const Footer = () => {
+//   return (
+//     <div className="w-full h-80 px-[min(10vw,120px)] flex justify-center items-center">
+//       <div className="relative overflow-hidden w-full h-full flex justify-end px-[min(10vw,12px)] text-left items-start py-12 border-t-1">
+//         <div className="flex flex-row space-x-12 sm:space-x-16 md:space-x-24 sm:text-lg md:text-xl">
+//           <div className="flex flex-col text-sm">
+//             <Link href="/contact" className="mb-1">
+//               <span className="link-text">Contact</span>
+//             </Link>
+//             <Link href="/privacy" className="mb-1">
+//               <span className="link-text">Privacy policy</span>
+//             </Link>
+//             <Link href="/terms" className="mb-1">
+//               <span className="link-text">Terms & Services</span>
+//             </Link>
+//           </div>
+
+//           <div className="flex flex-col text-sm">
+//             <Link
+//               href="https://www.github.com/ferdus615"
+//               target="_blank"
+//               className="mb-1"
+//             >
+//               <span className="link-text">Github</span>
+//             </Link>
+//             <Link
+//               href="https://www.linkedin.com/in/ferdus615"
+//               target="_blank"
+//               className="mb-1"
+//             >
+//               <span className="link-text">Linkedin</span>
+//             </Link>
+//             <Link href="#" target="_blank" className="mb-1">
+//               <span className="link-text">Instagram</span>
+//             </Link>
+//           </div>
+//         </div>
+
+//         <h2 className="absolute bottom-0 left-0 translate-y-1/3 text-[min(15vw,192px)] text-rose">
+//           TabMaster
+//         </h2>
+//       </div>
+//     </div>
+//   );
+// };
+
 const Footer = () => {
   return (
-    <div className="w-full h-80 px-[min(10vw,120px)] flex justify-center items-center">
-      <div className="relative overflow-hidden w-full h-full flex justify-end px-[min(10vw,12px)] text-left items-start py-12 border-t-1">
-        <div className="flex flex-row space-x-12 sm:space-x-16 md:space-x-24 sm:text-lg md:text-xl">
-          <div className="flex flex-col text-sm">
-            <Link href="/contact" className="mb-1">
-              <span className="link-text">Contact</span>
-            </Link>
-            <Link href="/privacy" className="mb-1">
-              <span className="link-text">Privacy policy</span>
-            </Link>
-            <Link href="/terms" className="mb-1">
-              <span className="link-text">Terms & Services</span>
-            </Link>
-          </div>
-
-          <div className="flex flex-col text-sm">
-            <Link
-              href="https://www.github.com/ferdus615"
-              target="_blank"
-              className="mb-1"
-            >
-              <span className="link-text">Github</span>
-            </Link>
-            <Link
-              href="https://www.linkedin.com/in/ferdus615"
-              target="_blank"
-              className="mb-1"
-            >
-              <span className="link-text">Linkedin</span>
-            </Link>
-            <Link href="#" target="_blank" className="mb-1">
-              <span className="link-text">Instagram</span>
-            </Link>
-          </div>
-        </div>
-
-        <h2 className="absolute bottom-0 left-0 translate-y-1/3 text-[min(15vw,192px)] text-rose">
-          TabMaster
-        </h2>
+    <div>
+      <div>
+        <h1>TabMaster</h1>
       </div>
+      <div></div>
     </div>
   );
 };
