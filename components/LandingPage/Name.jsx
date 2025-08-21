b582c92f8830a9c4432c2aaf9d204c88637b705b
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-08-21 07:57:57 +0600

    name style

--- components/LandingPage/Name.jsx
+++ components/LandingPage/Name.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Name = () => {
   return (
     <div className="h-auto flex flex-col items-center">
-      <span className="font-extralight text-sm rounded-full border px-3 py-1 bg-gradient">
+      <span className="font-extralight text-sm rounded-full border px-5 py-1 bg-gradient">
         The Future of Debate Management
       </span>
       <span className="text-[min(15vw,200px)]/30 md:text-[min(15vw,200px)]/60 font-normal tracking-tighter">
