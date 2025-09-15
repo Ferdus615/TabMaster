5abaa0110ca5bb543d6ba60b08f6b6bf2dc7c6c5
ferdus615 <ferdus615@gmail.com>
2025-09-15 21:19:35 +0600

    leaderboard page header done

--- components/DemoPages/Leaderboard.jsx
+++ components/DemoPages/Leaderboard.jsx
@@ -5,17 +5,41 @@ const Leaderboard = () => {
   return (
     <div>
       {/* Header tab */}
+      {/* Header tab */}
       <header className="bg-gray-900 rounded-lg p-3 flex flex-col sm:flex-row gap-5 sm:items-center justify-between">
-        <div className="flex ">
+        <div className="">
+          <span
+            className="border rounded-md px-3 py-1 text-colorText flex flex-wrap items-center gap-2 hover:cursor-pointer 
+                              hover:bg-gray-800"
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M20 12L5 12M10 6L5 12L10 18"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <p>Back</p>
+          </span>
+        </div>
+        <div className="flex">
           <Image
-            src="/feature_2-pic/trophy.svg"
+            src="/feature_2-pic/adjudicator.svg"
             height={20}
             width={20}
             alt="trophy"
             className="mr-3"
           />
           <p className="font-bold text-xl font-heading text-rose">
-            TabMaster Debate Tournament
+            Leaderboard
           </p>
         </div>
         <div className="flex items-center">
